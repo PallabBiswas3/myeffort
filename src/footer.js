def44f83c83cf8e7c49d548af7be0686e796b00b
PallabBiswas3 <pallabbiswas891@gmail.com>
2024-04-10 15:47:57 +0530

    added section id

--- src/footer.js
+++ src/footer.js
@@ -3,14 +3,16 @@ import './footer.css';
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="contact-info">
-                <p>Name: Book Store</p>
-                <p>Email: example@example.com</p>
-                <p>Phone: 123-456-7890</p>
-            </div>
-            <p>© 2024 Book Store. All Rights Reserved.</p>
-        </footer>
+        <section id="footer">
+            <footer>
+                <div className="contact-info">
+                    <p>Name: Book Store</p>
+                    <p>Email: example@example.com</p>
+                    <p>Phone: 123-456-7890</p>
+                </div>
+                <p>© 2024 Book Store. All Rights Reserved.</p>
+            </footer>
+        </section>
     );
 }
 
--- src/home.js
+++ src/home.js
@@ -3,17 +3,19 @@ import './home.css';
 
 const Home = () => {
     return (
-        <div className="home">
-            <span className="background-image">
-                <img src={process.env.PUBLIC_URL + '/image/' + 'image.png'} alt="background" className="background-image" />
-            </span>
-            <span className="content">
-                <h1 className="home-heading">Book Shop Template</h1>
-                <p className="home-heading1">
-                    TEMPLATE FOR BOOKSTORES, COLLECTIBLES. Book Shop is a free eCommerce website template built with newest version of WordPress, WooCommerce, Elementor, and WooVina theme. It comes with a unique homepage layout that prominently displays categories and a variety of products in an arrangement that is popular with bookstores and collectibles.
-                </p>
-            </span>
-        </div>
+        <section id="home">
+            <div className="home">
+                <span className="background-image">
+                    <img src={process.env.PUBLIC_URL + '/image/' + 'image.png'} alt="background" className="background-image" />
+                </span>
+                <span className="content">
+                    <h1 className="home-heading">Book Shop Template</h1>
+                    <p className="home-heading1">
+                        TEMPLATE FOR BOOKSTORES, COLLECTIBLES. Book Shop is a free eCommerce website template built with newest version of WordPress, WooCommerce, Elementor, and WooVina theme. It comes with a unique homepage layout that prominently displays categories and a variety of products in an arrangement that is popular with bookstores and collectibles.
+                    </p>
+                </span>
+            </div>
+        </section>
     );
 }
 
